b97d8137cc1c849bf2ef5ba4dc98201d0f2eda59
Chris Dueck <chrisduckling@gmail.com>
2026-04-15 18:21:05 +0100

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chrisdueck.github.io',
+  site: 'https://chrisdueck.net',
   integrations: [tailwind(), astroIcon({
     include: {
       mdi: ["*"],
